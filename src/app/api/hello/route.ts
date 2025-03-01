ec4341b284aa2b823db2a596ae391eba1fd02f90
Maxwell Tang <amoinus@gmail.com>
2025-03-01 14:50:17 -0500

    Added Example Route With Supabase

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,23 @@
+import { randomBytes } from 'crypto';
+import dotenv from 'dotenv'
+import { createClient } from '@supabase/supabase-js'
+
+dotenv.config()
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function GET(request: Request) {
+  const supabaseApiKey = process.env.SUPABASE_API_KEY;
+  const supabaseUrl = process.env.SUPABASE_URL;
+  if(!supabaseApiKey) throw new Error("No API key found!");
+  if(!supabaseUrl) throw new Error("No Supabase URL found!");
+
+  const client = createClient(supabaseUrl, supabaseApiKey);
+
+  client.storage
+    .from("hello-world")
+    .upload(
+      `${randomBytes(20).toString('base64url')}.txt`,
+      Buffer.from("Hello, world!", 'utf-8')
+    )
+  return new Response('Test');
+}
