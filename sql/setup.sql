473a51d6f0eb5d61f49784774534890e697a7ce6
ZhiyangWen <144734971+ZhiyangWen@users.noreply.github.com>
2025-04-26 16:17:21 -0400

    sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -18,12 +18,12 @@ CREATE TABLE photo (
 );
 
 CREATE TABLE tag (
-    name   VARCHAR(32) NOT NULL,
+    name   VARCHAR(32) PRIMARY KEY
 );
 
 CREATE TABLE phototag (
     photoid  INTEGER REFERENCES photo(id) NOT NULL,
-    tagname  VARCHAR REFERENCES tag(name) NOT NULL
+    tag      VARCHAR(32) REFERENCES tag(name) NOT NULL
 );
 
 CREATE TABLE event (
@@ -31,5 +31,5 @@ CREATE TABLE event (
     name      VARCHAR(64) NOT NULL,
     startdate DATE NOT NULL,
     enddate   DATE NOT NULL,
-
+    tag       VARCHAR(32) REFERENCES tag(name) NOT NULL
 );
