1400682aa20f164bc143461c399ce800d1758b0e
Sodiumtelluride <gelfandnat@gmail.com>
2025-04-26 15:51:55 -0400

    worked on setting up sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -11,3 +11,25 @@ CREATE TABLE blog (
     authorid INTEGER REFERENCES photoclubuser(id) NOT NULL,
     file     VARCHAR(128) NOT NULL
 );
+
+CREATE TABLE photo (
+    id       SERIAL PRIMARY KEY, 
+    file     VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE tag (
+    name   VARCHAR(32) NOT NULL,
+);
+
+CREATE TABLE phototag (
+    photoid  INTEGER REFERENCES photo(id) NOT NULL,
+    tagname  VARCHAR REFERENCES tag(name) NOT NULL
+);
+
+CREATE TABLE event (
+    id        SERIAL PRIMARY KEY,
+    name      VARCHAR(64) NOT NULL,
+    startdate DATE NOT NULL,
+    enddate   DATE NOT NULL,
+
+);
